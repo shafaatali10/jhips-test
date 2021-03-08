63bd0d3e939e5ec3cab289bfb372f7f9f875b667
shafaat <ali.khan.shafaat@ncs.com.sg>
2021-03-08 09:41:29 +0800

    Jhipster Base

---
+++ src/main/java/com/shafaat/jhips/JhipsTestApplication.java
@@ -0,0 +1,13 @@
+package com.shafaat.jhips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhipsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhipsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shafaat/jhips/JhipsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shafaat.jhips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhipsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
